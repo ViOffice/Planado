90972d3d7765223824336cd1d9caf81ea71a23cc
Jan Weymeirsch <dev@vioffice.de>
2021-04-12 14:53:57 +0200

    Simpler build_html()

--- lib/html.php
+++ lib/html.php
@@ -3,37 +3,25 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-only
 
-function build_html($content, $title, $description) {
+function build_html($content) {
 
-    // Define common HTML --------------------------------------------------- //
+    // Echo common HTML ------------------------------------------------------- //
+    echo "<html>
+            <head>
+			  <meta charset='utf-8'>
+			  <title>" . $website_title . "</title>
+			  <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>
+			  <meta name='description' content=" . $website_description . ">
+			  <link rel='stylesheet' href='static/css/default.css'>
+			  <link rel='stylesheet' href='static/css/custom.css'>
+			  <link rel='shortcut icon' href='static/img/favicon.ico' type='image/x-icon'>
+			  <script src='static/js/custom.js'></script>
+			</head>
+			<body id='body'>
+			  <div id='content'>" . $content . "</div>
+			</body>
+          </html>";
 
-    $html_head="<html>
-                  <head>
-                    <meta charset='utf-8'>
-                    <title>" . $title . "</title>
-                    <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>
-                    <meta name='description' content='" . $description . "'>
-                    <link rel='stylesheet' href='static/css/default.css'>
-                    <link rel='stylesheet' href='static/css/custom.css'>
-                    <link rel='shortcut icon' href='static/img/favicon.ico' type='image/x-icon'>
-                    <script src='static/js/custom.js'></script>
-                  </head>";
-
-    $html_prec="  <body id='body'>
-                    <div id='content'>";
-
-    // -- Website Content comes here -- //
-
-    $html_posc="    </div><!--content-->
-                  </body>
-                </html>";
-
-    // Print HTML ----------------------------------------------------------- //
-
-    echo $html_head;
-    echo $html_prec;
-    echo $content;
-    echo $html_posc;
 };
 
 ?>
